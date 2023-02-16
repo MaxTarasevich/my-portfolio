90ed29a35ac8365ca2fd3d4ea07dfc0ffdfd7d34
Max <maxtarasevich@gmail.com>
2023-02-16 21:00:38 +0300

    feat: add sidebar

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../Sidebar'
+
+import './index.scss'
+
+const Layout = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default Layout
